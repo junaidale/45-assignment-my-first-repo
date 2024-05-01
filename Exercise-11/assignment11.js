e5ddba8eec8afc6f05d6c2660eac8ad1471e8f55
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-01 10:40:56 -0700

    add javascript file assignment11.js

---
+++ Exercise-11/assignment11.js
@@ -0,0 +1,4 @@
+/*Names: Store the names of a few of your friends in an array called names.
+Print each person’s name by accessing each element in the list, one at a time.*/
+var friendsName = ["Hamza", "Umar", "Amir"];
+friendsName.forEach(function (friend) { return console.log(friend); });
