6767b888692cee4f0cf8310894c211978735ebba
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-01 10:31:52 -0700

    Create and complete assignment9.ts

---
+++ Exercise-9/assignment9.ts
@@ -0,0 +1,7 @@
+/*Favorite Number: Store your favorite number in a variable. Then, 
+create a message revealing your favorite number. Print that message.*/
+
+
+let favouriteNumber = 8;
+let message = "My favourite number is:";
+console.log(message, favouriteNumber);
