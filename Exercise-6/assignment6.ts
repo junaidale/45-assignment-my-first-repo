9b639e782ba0387f87d92555320e5f704856328f
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-01 10:22:25 -0700

    Create and complete assignment6.ts

---
+++ Exercise-6/assignment6.ts
@@ -0,0 +1,11 @@
+/*Stripping Names: Store a person’s name, 
+and include some whitespace characters at the beginning and end of the name. 
+Use "\t" (tab) and "\n" (new line) at least once. Print the name once, 
+so the whitespace around the name is displayed. Then print the name after stripping the white spaces.*/
+
+
+let whiteSpaceName = "\n\t Junaid Ahmed \t\n";
+console.log(whiteSpaceName);
+
+let withoutWhiteSpaceName = whiteSpaceName.trim();
+console.log(withoutWhiteSpaceName);
