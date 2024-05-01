4c11011669712898bf9db9f138fccd7961d067ee
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-01 10:19:16 -0700

    add javascript file assignment5.js

---
+++ Exercise-5/assignment5.js
@@ -0,0 +1,7 @@
+/*Famous Quote 2: Repeat Exercise 1, but this time store the
+famous person’s name in a variable called famous_person.
+Then compose your message and store it in a new variable called message.
+Print your message.*/
+var famousPerson = "Albert Einstine";
+var message = "Once said, \"A person who never made a mistake never tried anything more new\"";
+console.log(famousPerson, message);
