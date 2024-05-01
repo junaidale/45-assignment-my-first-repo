a78612b3b5f73bd3304cbe44fad1160acf033c78
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-01 10:37:48 -0700

    add javascript file assignment10.js

---
+++ Exercise-10/assignment10.js
@@ -0,0 +1,11 @@
+/*Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+If the programs are straightforward at this point, just add your name and the current date
+at the top of each program file. Then, write one sentence describing what the program does.*/
+//Addition
+console.log(4 + 4);
+//Subtraction
+console.log(12 - 4);
+//Multiplication
+console.log(4 * 2);
+//Division
+console.log(16 / 2);
