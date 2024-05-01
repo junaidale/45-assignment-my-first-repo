5157e43065941b765d4e48eb69fd50e4b2c62917
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-01 10:40:03 -0700

    Create and complete assignment11.ts

---
+++ Exercise-11/assignment11.ts
@@ -0,0 +1,6 @@
+/*Names: Store the names of a few of your friends in an array called names. 
+Print each person’s name by accessing each element in the list, one at a time.*/
+
+
+let friendsName = ["Hamza", "Umar", "Amir"];
+friendsName.forEach(friend => console.log(friend));
