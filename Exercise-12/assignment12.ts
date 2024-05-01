84e464de0cdc2b7b33e2bf2edddf46671fb7add9
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-01 10:43:35 -0700

    Create and complete assignment12.ts

---
+++ Exercise-12/assignment12.ts
@@ -0,0 +1,6 @@
+/*Greetings: Use the array from Exercise 11. Instead of just printing each person’s name, 
+print a message to them. The message should be the same for each person, but personalized with their name.*/
+
+
+let friendsName = ["Hamza", "Umar", "Amir"];
+friendsName.forEach(friendName => console.log(`Hello ${friendName}, How are you`));
