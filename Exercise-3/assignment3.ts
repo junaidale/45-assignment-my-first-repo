a969ecb0e8be4ca60ab5b078d43758186758a386
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-01 10:06:35 -0700

    update and complete assignment3.ts

--- Exercise-3/assignment3.ts
+++ Exercise-3/assignment3.ts
@@ -1,1 +1,16 @@
+/*Name Cases: Store a person’s name in a variable,
+ and then print that person’s name in lowercase, uppercase, and titlecase.*/
+ 
+
+let myName = "Junaid Ahmed";
+
+//UpperCase
+console.log(myName.toUpperCase());
+
+//LowerCase
+console.log(myName.toLowerCase());
+
+//TitleCase
+console.log(myName.charAt(0).toUpperCase() + 
+myName.slice(1).toLowerCase());
 
