0df8fb20719a1d45097e0887891b37ed183c2a11
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-01 10:18:12 -0700

    Create and complete assignment5.ts

---
+++ Exercise-5/assignment5.ts
@@ -0,0 +1,9 @@
+/*Famous Quote 2: Repeat Exercise 1, but this time store the 
+famous person’s name in a variable called famous_person. 
+Then compose your message and store it in a new variable called message. 
+Print your message.*/
+
+
+let famousPerson = "Albert Einstine";
+let message = "Once said, \"A person who never made a mistake never tried anything more new\"";
+console.log(famousPerson, message);
