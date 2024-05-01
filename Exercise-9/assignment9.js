4f4667068fd41820d89e7ad8ac7d85b70ce7e9bd
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-01 10:33:10 -0700

    add javascript file assignment9.js

---
+++ Exercise-9/assignment9.js
@@ -0,0 +1,5 @@
+/*Favorite Number: Store your favorite number in a variable. Then,
+create a message revealing your favorite number. Print that message.*/
+var favouriteNumber = 8;
+var message = "My favourite number is:";
+console.log(message, favouriteNumber);
