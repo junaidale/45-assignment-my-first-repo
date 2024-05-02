6ab62aac4aabf8fe47247844bb57e2a21edd2f4b
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-02 00:41:17 -0700

    Create and complete assignment19.ts

---
+++ Exercise-19/assignment19.ts
@@ -0,0 +1,12 @@
+/*Dinner Guests: Use one of your programs from Exercises 14 through 18 to print a message 
+indicating the number of people you are inviting to dinner.*/
+
+// Guest list from exercise 14
+let guestList = ["Summaiya", "Hania", "Hoorain", "Shahzain"];
+
+//guestList.forEach(oneGuest => console.log(`Assalam-o-Alaikum ${oneGuest}, would you like to dinner with me?`));
+
+let lengthGuests: number = guestList.length;
+
+// Print the message for dinner invitation
+console.log(`We are Inviting total ${lengthGuests} guests.`);
