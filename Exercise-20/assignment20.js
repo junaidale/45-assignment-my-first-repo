d4ad83112b69ad6eb6a178a2c1dae9a853d8abd3
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-02 00:15:40 -0700

    add javascript file assignment20.js

---
+++ Exercise-20/assignment20.js
@@ -0,0 +1,39 @@
+/*Think of something you could store in an array. For example,
+you could make a list of countries, rivers, mountains,
+cities, languages, or anything else you’d like.
+Write a program that creates a list containing these items.*/
+//List of Countries
+var countries = ["China", "United States", "France", "United Kingdom", "Russia"];
+console.log("List of permanent members of Security Council:");
+for (var _i = 0, countries_1 = countries; _i < countries_1.length; _i++) {
+    var top_1 = countries_1[_i];
+    console.log(top_1);
+}
+//List of Rivers
+var rivers = ["Nile", "Amazon River", "Yenisei River", "Yangtze River", "Mississippi River"];
+console.log("List of top 5 Longest rivers in the World:");
+for (var _a = 0, rivers_1 = rivers; _a < rivers_1.length; _a++) {
+    var top_2 = rivers_1[_a];
+    console.log(top_2);
+}
+//List of Mountains
+var mountains = ["Mount Everest", "K2", "Lhotse", "Makalu", "Cho Oyu"];
+console.log("List of top 5 highest Mountains in the World:");
+for (var _b = 0, mountains_1 = mountains; _b < mountains_1.length; _b++) {
+    var top_3 = mountains_1[_b];
+    console.log(top_3);
+}
+//List of Cities
+var cities = ["Paris", "Cape Town", "Prague", "Venice", "Istanbul"];
+console.log("List of top 5 most beautiful cities in the World:");
+for (var _c = 0, cities_1 = cities; _c < cities_1.length; _c++) {
+    var top_4 = cities_1[_c];
+    console.log(top_4);
+}
+//List of Languages
+var languages = ["English", "Mandarin", "Spanish", "Arabic", "Hindi/Urdu"];
+console.log("List of 5 languages to most spoken in the World:");
+for (var _d = 0, languages_1 = languages; _d < languages_1.length; _d++) {
+    var top_5 = languages_1[_d];
+    console.log(top_5);
+}
