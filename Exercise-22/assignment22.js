1588872844947332a4c07ddd04c3e9c542fd95a1
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-02 00:19:44 -0700

    add javascript file assignment22.js

---
+++ Exercise-22/assignment22.js
@@ -0,0 +1,8 @@
+/*Intentional Error: Try to produce an array index error in one of your programs.
+Correct the error before finishing*/
+// Creating a Array
+var errorArray = ["A", "B", "C", "D"];
+// Producing Error ! by Accessing invalid index of array
+// console.log(errorArray[10]);
+// Error Removed
+console.log(errorArray[1]);
