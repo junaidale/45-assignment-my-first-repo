addb6650a871de81c29424da2f485394698ee883
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-02 00:18:55 -0700

    Create and complete assignment22.ts

---
+++ Exercise-22/assignment22.ts
@@ -0,0 +1,12 @@
+/*Intentional Error: Try to produce an array index error in one of your programs.
+Correct the error before finishing*/
+
+
+// Creating a Array
+let errorArray: string[] =["A", "B", "C", "D"];
+
+// Producing Error ! by Accessing invalid index of array
+// console.log(errorArray[10]);
+
+// Error Removed
+console.log(errorArray[1]);
