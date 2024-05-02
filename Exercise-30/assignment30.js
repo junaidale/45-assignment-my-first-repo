e61d2cec42ccdb4f28ffb76581d47286b46beaf0
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-02 00:35:32 -0700

    add javascript file assignment30.js

---
+++ Exercise-30/assignment30.js
@@ -0,0 +1,11 @@
+// Hello Admin: Greet users differently, especially 'admin'.
+// Make array of usernames
+var userNames = ["admin", "faran", "shahzain", "zohan", "hadi", "saad"];
+for (var i = 0; i < userNames.length; i++) {
+    if (userNames[i] === 'admin') {
+        console.log("Hello admin, would you like to see a status report?");
+    }
+    else {
+        console.log("Hello ".concat(userNames[i], ", thank you for logging in again."));
+    }
+}
