9cdfcbd913215d8d4cab97b706e61e49f9236b0d
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-02 00:24:55 -0700

    Create and complete assignment25.ts

---
+++ Exercise-25/assignment25.ts
@@ -0,0 +1,15 @@
+/*Alien Colors #1: Imagine an alien was just shot down in a game. Assign 'green', 'yellow', 
+or 'red' to a variable called alien_color.Alien Colors #1: Imagine an alien was just shot down in a game. 
+Assign 'green', 'yellow', or 'red' to a variable called alien_color.*/
+
+
+// Create a varible called alien_color
+let alien_color ="green";
+
+if(alien_color === "green"){
+    // Code Block
+    console.log("Alien color is green player just earned 5 points.");
+}
+if (alien_color === "red"){
+    console.log("Alien color is red");
+}
