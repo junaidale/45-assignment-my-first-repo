692f5be955e622d63f2ea0a0b7ed767976fdde86
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-02 00:32:56 -0700

    Create and complete assignment29.ts

---
+++ Exercise-29/assignment29.ts
@@ -0,0 +1,31 @@
+// Favorite Fruit: Create an array for your favorite fruits and check if certain fruits are included.
+
+
+// Define Array of Favorite Fruits
+let favorite_fruits: string[] = ["grapes", "strawberry", "apple"];
+
+// Making Array of Favorite Fruits
+if (favorite_fruits.includes("grapes"))
+{
+    console.log("I really like grapes!");
+}
+
+if(favorite_fruits.includes("strawberry"))
+{
+    console.log("I really like strawberry!");
+}
+
+if (favorite_fruits.includes("apple"))
+{
+    console.log("I really like apple!");
+}
+
+if (favorite_fruits.includes("orange"))
+{
+    console.log("I really like orange!");
+}
+
+if (favorite_fruits.includes("peach"))
+{
+    console.log("I really like peach!");
+}
