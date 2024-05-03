198944ee648952a97c8d96ebcdf448178fa671b8
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-02 22:53:20 -0700

    add javascript file assignment38.js

---
+++ Exercise-38/assignment38.js
@@ -0,0 +1,10 @@
+//Cities: Describing cities with a function
+// Write a function called describe_city() that accepts the name of a city and its country.
+function describe_city(city, country) {
+    if (country === void 0) { country = "Pakistan"; }
+    // Function should print a simple sentence
+    console.log("".concat(city, " is in ").concat(country, ". "));
+}
+describe_city("Karachi");
+describe_city("Peshawar");
+describe_city("Tokyo", "Japan");
