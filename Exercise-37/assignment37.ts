85afef5efa7b8a5c0a6daa0655dd6ebd49341d29
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-02 22:50:33 -0700

    Create and complete assignment37.ts

---
+++ Exercise-37/assignment37.ts
@@ -0,0 +1,9 @@
+// Large Shirts: Default values in make_shirt().
+
+
+function make_shirt(size = "Large", message = "I Love Typescript!"):void{
+    console.log(`I'm making a ${size} T-Shirt with the message"${message}".`);
+}
+make_shirt("Large")
+make_shirt("Medium")
+make_shirt("Small", "I Love Pakistan!");
