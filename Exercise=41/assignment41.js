01f1d42b89ce716d57c0c34e766376c21a9e735e
junaidale <160826852+junaidale@users.noreply.github.com>
2024-05-02 22:58:51 -0700

    add javascript file assignment41.js

---
+++ Exercise=41/assignment41.js
@@ -0,0 +1,10 @@
+//Magicians: Display magician names from an array.
+// Make a array called show magician
+function show_magicians(magicians) {
+    for (var i = 0; i < magicians.length; i++) {
+        // Print the name of each magician
+        console.log(magicians[i]);
+    }
+}
+var magicians = ['Ali raza', 'Afzaal Afridi', 'Sabir Ali', 'Shahrukh Zahoor'];
+show_magicians(magicians);
